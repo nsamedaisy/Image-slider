7400661a549b314afcfcf9cc2529301479b32419
nsamedaisy <nsamedaisy@gmail.com>
2023-05-02 15:04:41 +0100

    fixed indentation

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
+
 function currentSlide(n) {
   showSlides(slideIndex = n)
 }
+
 function showSlides(n) {
   let i;
-  var slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  var slides = document.getElementsByClassName('slides');
+  if (n > slides.length) {
+    slideIndex = 1
   }
-  slides[slideIndex - 1].style.display = "block";
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
 }
